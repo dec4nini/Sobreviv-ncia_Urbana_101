8c14115fe2715b3215ddd8dd74f238df8cf0755b
Eduardo Decanini <eduardodecanini@gmail.com>
2021-08-30 22:19:02 -0300

    Camera subsituída por script manual, adicionados novos tilemaps

---
+++ Assets/Scripts/CameraMovement2D.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public float timeoffset;
+    public Vector2 positOffset;
+    private Vector3 velicity;
+    public float leftlimit;
+    public float rigthlimit;
+    public float bottomlimit;
+    public float toplimit;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //posição inicial da câmera
+        Vector3 startPos = transform.position;
+
+        //Posição atual do jogador
+        Vector3 endPos = player.transform.position;
+        endPos.x += positOffset.x;
+        endPos.y += positOffset.y;
+        endPos.z = -10;
+
+        //transform.position = Vector3.Lerp(startPos, endPos, timeoffset * Time.deltaTime);
+
+        //smooth damp:
+        transform.position = Vector3.SmoothDamp(startPos, endPos, ref velicity, timeoffset);
+
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, leftlimit, rigthlimit),
+            Mathf.Clamp(transform.position.y, bottomlimit, toplimit),
+            transform.position.z
+        );
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        //"desenha" linhas ao redor da câmera
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftlimit, toplimit), new Vector2(rigthlimit, toplimit));
+        Gizmos.DrawLine(new Vector2(rigthlimit, toplimit), new Vector2(rigthlimit, bottomlimit));
+        Gizmos.DrawLine(new Vector2(rigthlimit, bottomlimit), new Vector2(leftlimit, bottomlimit));
+        Gizmos.DrawLine(new Vector2(leftlimit, bottomlimit), new Vector2(leftlimit, toplimit));
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/NPC_Movement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Movement : MonoBehaviour
+{
+    public float decisionTimeCount = 0;
+    public int randomDirection;
+    public float movespeed;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    private Animator anima;
+    private string currentAnimation;
+    //awake are only called when all objects are initialized
+    private void Awake()
+    {
+        decisionTimeCount = Random.Range(4, 7);
+        rb = GetComponent<Rigidbody2D>();
+        anima = gameObject.GetComponent<Animator>();
+        anima.Play(PLAYER_IDLE);
+    }
+
+    //Animation States
+    const string PLAYER_IDLE = "Player_Idle";
+    const string PLAYERWALK = "PlayerWalk";
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get inputs
+        ProcessInputs();
+        //Animate
+        Animate();
+        //Move
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * movespeed, rb.velocity.y);
+    }
+
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+
+            FlipChar();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipChar();
+        }
+        else if (moveDirection != 0)
+        {
+            anima.Play(PLAYERWALK);
+        }
+        else
+        {
+            anima.Play(PLAYER_IDLE);
+        }
+
+    }
+
+    private void ProcessInputs()
+    {
+
+        if (decisionTimeCount > 0)
+        {
+            decisionTimeCount -= Time.deltaTime;
+        }
+        else
+        {
+            if (moveDirection >= 0)
+            {
+                moveDirection = -1;
+            }
+            else
+            {
+                moveDirection = 1;
+            }
+
+            decisionTimeCount = 3;
+        }
+    }
+
+    private void FlipChar()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
+
