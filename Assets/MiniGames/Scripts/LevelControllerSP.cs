557075dac2e9b4a274c5e7223e0a994c40c12c51
LuisFelipeagarcia <luisfelipeagarcia@gmail.com>
2021-09-03 14:20:13 -0300

    criação do sistema de sequencias no minigame

--- Assets/MiniGames/Scripts/LevelControllerSP.cs
+++ Assets/MiniGames/Scripts/LevelControllerSP.cs
@@ -1,18 +1,135 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+[Serializable]
+public class KeysSP
+{
+    public Sprite keySprite;
+    public KeyCode key;
+}
 
 public class LevelControllerSP : MonoBehaviour
 {
+    public static LevelControllerSP instance;
+    public KeysSP[] keysSP;
+
+    public List<KeysSP> gameKeysSP;
+    public List<KeysSP> gameKeysSP2;
+    public List<KeysSP> gameKeysSP3;
+    public Image[] player1Images;
+    
+    public Text messageText;
+    public Text playerTimeText;
+    public float playerTime;
+
+    public PlayerControllerSP playerSP;
+
+    public bool canPlay = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(StartingKeys(1));
     }
 
     // Update is called once per frame
-    void Update()
+
+    IEnumerator StartingKeys(int id)
+    {
+        if (id == 1) 
+        {
+            for (int i = 0; i < player1Images.Length; i++)
+            {
+
+                gameKeysSP.Add(keysSP[UnityEngine.Random.Range(0, keysSP.Length)]);
+                gameKeysSP2.Add(keysSP[UnityEngine.Random.Range(0, keysSP.Length)]);
+                gameKeysSP3.Add(keysSP[UnityEngine.Random.Range(0, keysSP.Length)]);
+            }
+        }
+        
+
+        for (int i = 0; i < player1Images.Length; i++)
+        {
+
+            if (id == 1)
+            {
+                player1Images[i].sprite = gameKeysSP[i].keySprite;
+                player1Images[i].preserveAspect = true;
+                player1Images[i].enabled = true;
+
+                yield return new WaitForSeconds(0.25f);
+            }
+            else if (id == 2)
+            {
+                player1Images[i].sprite = gameKeysSP2[i].keySprite;
+                player1Images[i].preserveAspect = true;
+                player1Images[i].enabled = true;
+
+                yield return new WaitForSeconds(0.25f);
+            }
+            else if (id == 3) 
+            {
+                player1Images[i].sprite = gameKeysSP3[i].keySprite;
+                player1Images[i].preserveAspect = true;
+                player1Images[i].enabled = true;
+
+                yield return new WaitForSeconds(0.25f);
+            }
+            
+        }
+        
+        
+        canPlay = true;
+        messageText.text = "Go";
+        StartCoroutine(Fading(messageText));
+    }
+
+    IEnumerator Fading(Text text)
+    {
+        Color newColor = text.color;
+        while (newColor.a > 0)
+        {
+            newColor.a -= Time.deltaTime;
+            text.color = newColor;
+            yield return null;
+        }
+    }
+
+    public void NextKey(int playerIndex, int keyIndex)
     {
         
+        if (playerIndex == 0)
+        {
+            player1Images[keyIndex].enabled = false;
+        }
+        
+    }
+    public void RestartSequencia(int id) 
+    {
+        StartCoroutine(StartingKeys(id));
+    }
+
+    public void UpdatePlayerTime(float time)
+    {
+        playerTime = time;
+
+        if (playerTime > 0)
+        {
+            playerTimeText.text = playerTime.ToString("0.00") + "s";
+        }
+    }
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/MiniGames/Scripts/PlayerControllerSP.cs
+++ Assets/MiniGames/Scripts/PlayerControllerSP.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class PlayerControllerSP : MonoBehaviour
 {
+    public Animator anim;
+    public int id = 1;
+    private int keyIndex = 4;
+    private bool canPlay = false;
+    public int sequenciaNumero;
+    private float timer;
+    private float nextkeyTime;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        while (LevelControllerSP.instance.canPlay == false)
+        {
+            yield return null;
+        }
+
+        canPlay = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (canPlay == false)
+        {
+            return;
+        }
+
+        timer += Time.deltaTime;
+
+        if (id == 1)
+        {
+            if (Input.GetKeyDown(LevelControllerSP.instance.gameKeysSP[keyIndex].key))
+            {
+                keyPress();
+            }
+            else if (Input.anyKeyDown)
+            {
+                timer += 0.1f;
+            }
+        }
+        else if (id == 2)
+        {
+            if (Input.GetKeyDown(LevelControllerSP.instance.gameKeysSP2[keyIndex].key))
+            {
+                keyPress();
+            }
+            else if (Input.anyKeyDown)
+            {
+                timer += 0.1f;
+            }
+        } else if(id == 3)
+        {
+            if (Input.GetKeyDown(LevelControllerSP.instance.gameKeysSP3[keyIndex].key))
+            {
+                keyPress();
+            }
+            else if (Input.anyKeyDown)
+            {
+                timer += 0.1f;
+            }
+        }
         
     }
+
+    void keyPress()
+    {
+        LevelControllerSP.instance.NextKey(0, keyIndex);
+        keyIndex--;
+
+        if (keyIndex < 0 && sequenciaNumero == 0)
+        {
+            canPlay = false;
+            LevelControllerSP.instance.UpdatePlayerTime(timer);
+        }
+        else if (keyIndex < 0)
+        {
+            id++;
+            sequenciaNumero--;
+            keyIndex = 4;
+            LevelControllerSP.instance.RestartSequencia(id);
+        }
+    }
+
+    public void SetAnimation(string triggerAnimation)
+    {
+        anim.SetTrigger(triggerAnimation);
+
+    }
+
 }
